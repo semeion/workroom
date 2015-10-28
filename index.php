cbbe898a464a067fd176bb598141862cf2e2e928
semeion <semeion7@hotmail.com>
2015-10-28 12:31:49 -0200

    bug with monolog class fixed

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
  * Time: 18:56
  */
 
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
 echo "testando";
 
 phpinfo();
